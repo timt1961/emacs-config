02f2fdcd35fe17404061db50e6a4db78dd79957f
Tim <tim.timmerman@gmail.com>
2020-03-29 12:47:48 +0200

    Stripped out most things from my config

--- lisp/vcs.el
+++
@@ -1,8 +0,0 @@
-(defun vcs-co-buffer ()
-  "Check out current buffer"
-  (interactive "")
-  (setq cmdline (concat "vcs co " buffer-file-name))
-  (shell-command cmdline)
-  (revert-buffer t t)
-  (kill-buffer "*Shell Command Output*")
-  (message (concat "Checked out file" buffer-file-name)))
